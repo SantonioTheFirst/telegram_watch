ac0741b8c50bc7641e9e1bda0e295f0dd1643927
SantonioTheFirst <santoniothefirst123@gmail.com>
2023-03-23 20:45:56 +0200

    cleared code

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from telethon.sessions import StringSession
 from config import api_id, api_hash, phone, session
 import os
 
-print('import ok')
 
 # with TelegramClient(StringSession(session), api_id, api_hash) as client:
 #     print(client.session.save()) # generate and save string session -- need for the first run
@@ -18,22 +17,14 @@ print('import ok')
 def main():
     now = datetime.now().strftime('%H:%M')
     pwd = os.path.dirname(os.path.realpath(__file__))
-    print(pwd)
-    print('now ok')
-
     with TelegramClient(StringSession(session), api_id=api_id, api_hash=api_hash).start() as client:
-        print('session ok')
         client(DeletePhotosRequest(client.get_profile_photos('me')))
-        print('delete ok')
         file = client.upload_file(f'{pwd}/time_images/{now}.jpg')
-        print(type(file))
         client(UploadProfilePhotoRequest(file))
-        print('update ok')
         client(UpdateProfileRequest(
-            first_name = f"{now}",
-            about = f"{now} "
+            first_name = f"{now}"
         ))
 
 
-main()
-print('end')
+if __name__ == '__main__':
+	main()
